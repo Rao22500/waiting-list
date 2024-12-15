b7d03f07a1214ea4a4afd4ea9d8841a16a3a2b71
Rao22500 <162340515+Rao22500@users.noreply.github.com>
2024-12-16 00:06:36 +0530

    Update utils/icons.tsx [BoltSync]

---
+++ utils/icons.tsx
@@ -0,0 +1,21 @@
+export const EmailIcon = () => (
+  <svg
+    className="fill-current"
+    xmlns="http://www.w3.org/2000/svg"
+    width={16}
+    height={14}
+  >
+    <path d="M14 0H2a2 2 0 0 0-2 2v10a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm0 12H2V5.723l5.504 3.145a.998.998 0 0 0 .992 0L14 5.723V12Zm0-8.58L8 6.849 2 3.42V2h12v1.42Z" />
+  </svg>
+);
+
+export const UserIcon = () => (
+  <svg
+    className="fill-current"
+    xmlns="http://www.w3.org/2000/svg"
+    width={16}
+    height={16}
+  >
+    <path d="M14.9 0c-.3 0-8.4.8-11.6 4-2.8 2.8-2.2 6.5-1.2 8.5L.3 14.3c-.4.4-.4 1 0 1.4.2.2.4.3.7.3.3 0 .5-.1.7-.3l1.8-1.8c.9.4 2.2.8 3.6.8 1.6 0 3.3-.5 4.9-2 3.4-3.4 4-11.3 4-11.6 0-.3-.1-.6-.3-.8-.2-.2-.5-.3-.8-.3Z" />
+  </svg>
+);
